0f3ad925018b3220e7cf462a92d1ae75169a0c08
Zerobandwidth <nimdavirus@gmail.com>
2021-03-12 09:49:07 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -769,16 +769,16 @@ function server_install_menu() {
 echo ""
 echo -ne "
 
-$(ColorOrange '----------------Server System Information-------------------')
-$(ColorOrange '-')$(ColorGreen '1)') Fresh or Reinstall Valheim Server
-$(ColorOrange '-')$(ColorGreen '0)') Go to Main Menu
-$(ColorOrange '------------------------------------------------------------')
-$(ColorPurple 'Choose an option:') "
+$(ColorOrange ''"$FUNCTION_SERVER_INSTALL_MENU_HEADER"'')
+$(ColorOrange '-')$(ColorGreen '1)') '"$FUNCTION_SERVER_INSTALL_MENU_OPT_1"'
+$(ColorOrange '-')$(ColorGreen '0)') '"$RETURN_MAIN_MENU"'
+$(ColorOrange ''"$DRAW60"'')
+$(ColorPurple ''"$CHOOSE_MENU_OPTION"'') "
         read a
         case $a in
 	        1) valheim_server_install ; server_install_menu ;;
            	    0) menu ; menu ;;
-		    *)  echo -ne " $(ColorRed 'Wrong option.')" ; server_install_menu ;;
+		    *)  echo -ne " $(ColorRed ''"$WRONG_MENU_OPTION"'')" ; server_install_menu ;;
         esac
 }
 ########################################################################
@@ -787,27 +787,29 @@ $(ColorPurple 'Choose an option:') "
 function display_system_info() {
 clear
 echo ""
-    echo -e "-------------------------------System Information----------------------------"
-    echo -e "Hostname:\t\t"`hostname`
-    echo -e "uptime:\t\t\t"`uptime | awk '{print $3,$4}' | sed 's/,//'`
-    echo -e "Manufacturer:\t\t"`cat /sys/class/dmi/id/chassis_vendor`
-    echo -e "Product Name:\t\t"`cat /sys/class/dmi/id/product_name`
-    echo -e "Version:\t\t"`cat /sys/class/dmi/id/product_version`
-    echo -e "Serial Number:\t\t"`cat /sys/class/dmi/id/product_serial`
-    echo -e "Machine Type:\t\t"`vserver=$(lscpu | grep Hypervisor | wc -l); if [ $vserver -gt 0 ]; then echo "VM"; else echo "Physical"; fi`
-    echo -e "Operating System:\t"`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`
-    echo -e "Kernel:\t\t\t"`uname -r`
-    echo -e "Architecture:\t\t"`arch`
-    echo -e "Processor Name:\t\t"`awk -F':' '/^model name/ {print $2}' /proc/cpuinfo | uniq | sed -e 's/^[ \t]*//'`
-    echo -e "Active User:\t\t"`w | cut -d ' ' -f1 | grep -v USER | xargs -n1`
-    echo -e "System Main IP:\t\t"`hostname -I`
+    echo -e "$DRAW80"
+    echo -e "$FUNCTION_DISPLAY_SYSTEM_INFO_HEADER"
+    echo -e "$FUNCTION_DISPLAY_SYSTEM_INFO_HOSTNAME"`hostname`
+    echo -e "$FUNCTION_DISPLAY_SYSTEM_INFO_UPTIME"`uptime | awk '{print $3,$4}' | sed 's/,//'`
+    echo -e "$FUNCTION_DISPLAY_SYSTEM_INFO_MANUFACTURER"`cat /sys/class/dmi/id/chassis_vendor`
+    echo -e "$FUNCTION_DISPLAY_SYSTEM_INFO_PRODUCT_NAME"`cat /sys/class/dmi/id/product_name`
+    echo -e "$FUNCTION_DISPLAY_SYSTEM_INFO_VERSION"`cat /sys/class/dmi/id/product_version`
+    echo -e "$FUNCTION_DISPLAY_SYSTEM_INFO_SERIAL_NUMBER"`cat /sys/class/dmi/id/product_serial`
+    echo -e "$FUNCTION_DISPLAY_SYSTEM_INFO_MACHINE_TYPE"`vserver=$(lscpu | grep Hypervisor | wc -l); if [ $vserver -gt 0 ]; then echo "VM"; else echo "Physical"; fi`
+    echo -e "$FUNCTION_DISPLAY_SYSTEM_INFO_OPERATION_SYSTEM"`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`
+    echo -e "$FUNCTION_DISPLAY_SYSTEM_INFO_KERNEL"`uname -r`
+    echo -e "$FUNCTION_DISPLAY_SYSTEM_INFO_ARCHITECTURE"`arch`
+    echo -e "$FUNCTION_DISPLAY_SYSTEM_INFO_PROCESSOR_NAME"`awk -F':' '/^model name/ {print $2}' /proc/cpuinfo | uniq | sed -e 's/^[ \t]*//'`
+    echo -e "$FUNCTION_DISPLAY_SYSTEM_INFO_ACTIVE_USER"`w | cut -d ' ' -f1 | grep -v USER | xargs -n1`
+    echo -e "$FUNCTION_DISPLAY_SYSTEM_INFO_SYSTEM_MAIN_IP"`hostname -I`
 echo ""
-    echo -e "-------------------------------CPU/Memory Usage------------------------------"
-    echo -e "Memory Usage:\t"`free | awk '/Mem/{printf("%.2f %%"), $3/$2*100}'`
-    echo -e "CPU Usage:\t"`cat /proc/stat | awk '/cpu/{printf("%.2f %%\n"), ($2+$4)*100/($2+$4+$5)}' |  awk '{print $0}' | head -1`
+    echo -e "$FUNCTION_DISPLAY_SYSTEM_INFO_CPU_MEM_HEADER"
+    echo -e "$FUNCTION_DISPLAY_SYSTEM_INFO_MEMORY_USAGE"`free | awk '/Mem/{printf("%.2f %%"), $3/$2*100}'`
+    echo -e "$FUNCTION_DISPLAY_SYSTEM_INFO_CPU_USAGE"`cat /proc/stat | awk '/cpu/{printf("%.2f %%\n"), ($2+$4)*100/($2+$4+$5)}' |  awk '{print $0}' | head -1`
 echo ""
-    echo -e "-------------------------------Disk Usage >80%-------------------------------"
+    echo -e "$FUNCTION_DISPLAY_SYSTEM_INFO_DISK_HEADER"
     df -Ph | sed s/%//g | awk '{ if($5 > 80) print $0;}'
+    echo -e "$DRAW80"
 echo ""
 }
 ########################################################################
